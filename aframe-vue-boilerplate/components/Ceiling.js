3f017291f09cb4a275eb24013b9bb77d3376d27e
aivivosa <aivi@designred.eu>
2019-05-17 11:53:07 +0300

    t6stsime failid ringi

--- aframe-vue-boilerplate/components/Ceiling.js
+++
@@ -1,9 +0,0 @@
-export default {
-   
-    template: `
-    <a-entity class"lagi">
-        <a-plane rotation="90 0 0" width="10" height="10" color="red"></a-plane>
-        <slot/>
-        </a-entity>
-        `   
-}
