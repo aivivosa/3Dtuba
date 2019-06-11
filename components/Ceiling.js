e7872e24a3c8284462f24a6943c0b78a7da8112a
aivivosa <aivi@designred.eu>
2019-06-11 15:16:09 +0300

    stiilid

--- components/Ceiling.js
+++ components/Ceiling.js
@@ -1,7 +1,7 @@
 export default {
    
     template: `
-    <a-entity class"lagi">
+    <a-entity class="lagi">
         <a-plane rotation="90 0 0" width="10" height="10" color="red"></a-plane>
         <slot/>
         </a-entity>
